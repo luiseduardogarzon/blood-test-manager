ef491ca195916bbded3b5e7bddffeea682262e37
luiseduardogarzon <luiseduardogarzon@gmail.com>
2022-03-17 15:13:51 -0500

    First Commit

---
+++ src/main/java/co/api/bloodtestmanager/BloodtestmanagerApplication.java
@@ -0,0 +1,13 @@
+package co.api.bloodtestmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodtestmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodtestmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/api/bloodtestmanager/BloodtestmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.api.bloodtestmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodtestmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
